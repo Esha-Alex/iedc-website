2fc4d226fc84727d19a7478681ba3828a9c18472
Esha Alex <eshaalexk@gmail.com>
2026-02-10 22:57:21 +0530

    execom

--- js/teamData.js
+++ js/teamData.js
@@ -2,21 +2,21 @@ window.teamData = [
     /* ============================
             CORE TEAM (Top Strip)
     ============================ */
-    { id: 1, name: "Christy", position: "CEO", photo: "assets/team/christy.jpg", team: "Core Team" },
+    { id: 1, name: "Christy", position: "CEO", photo: "assets/team/Christy.jpg", team: "Core Team" },
     { id: 2, name: "Mariya", position: "COO", photo: "assets/team/mariya.jpg", team: "Core Team" },
     { id: 3, name: "Kevin", position: "Secretary", photo: "assets/team/kevin.jpg", team: "Core Team" },
     { id: 4, name: "Abhishek", position: "CTO", photo: "assets/team/abhishek.jpg", team: "Core Team" },
     { id: 5, name: "Anirudh", position: "CAO", photo: "assets/team/anirudh.jpg", team: "Core Team" },
-    { id: 6, name: "Maria", position: "CFO", photo: "assets/team/maria.jpg", team: "Core Team" },
-    { id: 7, name: "Neeraj", position: "Marketing Co-Lead", photo: "assets/team/neeraj.jpg", team: "Core Team" },
-    { id: 8, name: "Malavika", position: "Marketing Co-Lead", photo: "assets/team/malavika.jpg", team: "Core Team" },
-    { id: 9, name: "Adhil", position: "Event Director", photo: "assets/team/adhil.jpg", team: "Core Team" },
+    { id: 6, name: "Maria", position: "CFO", photo: "assets/team/Maria Roselind.jpg", team: "Core Team" },
+    { id: 7, name: "Dheeraj", position: "Marketing Co-Lead", photo: "assets/team/Dheeraj.jpeg", team: "Core Team" },
+    { id: 8, name: "Malavika", position: "Marketing Co-Lead", photo: "assets/team/Malavika.jpeg", team: "Core Team" },
+    { id: 9, name: "Adhil", position: "Event Director", photo: "assets/team/Adhil Rasheed Director event.jpg", team: "Core Team" },
 
     /* ============================
              CMO & CCO
     ============================ */
-    { id: 10, name: "Haridev", position: "CMO", photo: "assets/team/haridev.jpg", team: "CMO & CCO" },
-    { id: 11, name: "Aslah", position: "CCO", photo: "assets/team/aslah.heif", team: "CMO & CCO" },
+    { id: 10, name: "Haridev", position: "CMO", photo: "assets/team/Haridev Ranjith Marketing.jpg", team: "CMO & CCO" },
+    { id: 11, name: "Aslah", position: "CCO", photo: "assets/team/aslah.jpg", team: "CMO & CCO" },
 
     /* ============================
              WEL & IIC
@@ -27,72 +27,78 @@ window.teamData = [
     /* ============================
              TBI & LOGI
     ============================ */
-    { id: 14, name: "Angel", position: "TBI Coordinator", photo: "assets/team/angel.jpg", team: "TBI & LOGI" },
-    { id: 15, name: "Gayathri", position: "TBI Co-Coordinator", photo: "assets/team/gayathri.jpg", team: "TBI & LOGI" },
-    { id: 16, name: "Anuvind", position: "Logistics Head", photo: "assets/team/anuvind.jpg", team: "TBI & LOGI" },
+    { id: 14, name: "Angel", position: "TBI Coordinator", photo: "assets/team/Angel Mary Mathew- TBI Incharge.jpg", team: "TBI & LOGI" },
+    { id: 15, name: "Gayathri", position: "TBI Co-Coordinator", photo: "assets/team/Gayathri S -TBI Incharge.jpg", team: "TBI & LOGI" },
+    { id: 16, name: "Anuvind", position: "Logistics Head", photo: "assets/team/Anuvind Babu.jpeg", team: "TBI & LOGI" },
+    { id: 17, name: "Dilrooba", position: "TBI", photo: "assets/team/Dilrooba faizal-TBI.jpg", team: "TBI & LOGI" },
+    { id: 18, name: "Ridha", position: "TBI", photo: "assets/team/Ridha latheef P K_TBI.jpg", team: "TBI & LOGI" },
 
     /* ============================
          SPONSORSHIP & STARTUP
     ============================ */
-    { id: 17, name: "Savio", position: "Marketing", photo: "assets/team/savio.jpg", team: "Sponsorship & Startup" },
-    { id: 18, name: "Samil", position: "Marketing Co-Lead", photo: "assets/team/samil.jpg", team: "Sponsorship & Startup" },
-    { id: 19, name: "Jeevan", position: "Sponsorship", photo: "assets/team/jeevan.jpg", team: "Sponsorship & Startup" },
-    { id: 20, name: "Shan", position: "Startup Team", photo: "assets/team/shan.jpg", team: "Sponsorship & Startup" },
-    { id: 21, name: "Anzal", position: "Startup Team", photo: "assets/team/anzal.jpg", team: "Sponsorship & Startup" },
+    { id: 19, name: "Savio", position: "Marketing", photo: "assets/team/Savio Saj.jpg", team: "Sponsorship & Startup" },
+    { id: 20, name: "Samil", position: "Marketing Co-Lead", photo: "assets/team/samil.jpg", team: "Sponsorship & Startup" },
+    { id: 21, name: "Jeevan", position: "Sponsorship", photo: "assets/team/Jeevan.JPEG", team: "Sponsorship & Startup" },
+    { id: 22, name: "Shan", position: "Startup Team", photo: "assets/team/SHAN AHAMMED BABU PT (STARTUP TEAM).jpg", team: "Sponsorship & Startup" },
+    { id: 23, name: "Anzal", position: "Startup Team", photo: "assets/team/anzal.jpg", team: "Sponsorship & Startup" },
 
     /* ============================
              DESIGN & DOC
     ============================ */
-    { id: 22, name: "Sanjana", position: "Design Lead", photo: "assets/team/sanjana.jpg", team: "Design & Doc" },
-    { id: 23, name: "Ameya", position: "Design Lead", photo: "assets/team/ameya.jpg", team: "Design & Doc" },
-    { id: 24, name: "Shelpa", position: "Doc Lead", photo: "assets/team/shelpa.jpg", team: "Design & Doc" },
+    
+    
+    
 
     // Design Team
-    { id: 25, name: "Ashika", position: "Design Team", photo: "assets/team/ashika.jpg", team: "Design Team" },
-    { id: 26, name: "Naseef", position: "Design Team", photo: "assets/team/naseef.jpg", team: "Design Team" },
+    { id: 24, name: "Aditya", position: "Design Lead", photo: "assets/team/aditya.jpeg", team: "Design & Doc" },
+    { id: 25, name: "Sanjana", position: "Design Lead", photo: "assets/team/Sanjana Induchoodan - design lead.jpg", team: "Design & Doc" },
+    { id: 26, name: "Ashika", position: "Design Team", photo: "assets/team/ashika.jpg", team: "Design Team" },
+    { id: 27, name: "Naseef", position: "Design Team", photo: "assets/team/Naseef Mohamme Nizamudeen.jpg", team: "Design Team" },
+    { id: 28, name: "Aayesh", position: "Design Team", photo: "assets/team/Aayesh.jpeg", team: "Design Team" },
+    { id: 29, name: "Anagha", position: "Design Team", photo: "assets/team/amogha.jpg", team: "Design Team" },
 
     // Doc Team
-    { id: 27, name: "Nandana", position: "Doc Team", photo: "assets/team/nandana.jpg", team: "Doc Team" },
-    { id: 28, name: "Navin", position: "Doc Team", photo: "assets/team/navin.jpg", team: "Doc Team" },
-    { id: 29, name: "Hannah", position: "Doc Team", photo: "assets/team/hannah.jpg", team: "Doc Team" },
-    { id: 30, name: "Avyish", position: "Doc Team", photo: "assets/team/avyish.jpg", team: "Doc Team" },
-    { id: 31, name: "Amogha", position: "Doc Team", photo: "assets/team/amogha.jpg", team: "Doc Team" },
-    { id: 32, name: "Samuel", position: "Doc Team", photo: "assets/team/samuel.jpg", team: "Doc Team" },
-    { id: 33, name: "Nidhi", position: "Doc Team", photo: "assets/team/nidhi.jpg", team: "Doc Team" },
+    { id: 30, name: "Shilpa", position: "Doc Lead", photo: "assets/team/shilpa nair-doc lead.jpg", team: "Design & Doc" },
+    { id: 31, name: "Nandana", position: "Doc Team", photo: "assets/team/Nandana A - DOC.jpg", team: "Doc Team" },
+    { id: 32, name: "Navin", position: "Doc Team", photo: "assets/team/Navin Ratnakumar-doc.png", team: "Doc Team" },
+    { id: 33, name: "Hannah", position: "Doc Team", photo: "assets/team/hannah.jpg", team: "Doc Team" },
+    
+    { id: 34, name: "Samuel", position: "Doc Team", photo: "assets/team/samuel.jpg", team: "Doc Team" },
+    { id: 35, name: "Nidhi", position: "Doc Team", photo: "assets/team/nidhi.jpg", team: "Doc Team" },
 
     /* ============================
               MEDIA & WEB
     ============================ */
     // Media
-    { id: 34, name: "Ansaf", position: "Media Lead", photo: "assets/team/ansaf.jpg", team: "Media Team" },
-    { id: 35, name: "Faraha", position: "Media Lead", photo: "assets/team/faraha.jpg", team: "Media Team" },
-    { id: 36, name: "Ayush", position: "Web Lead", photo: "assets/team/avilash.jpg", team: "Web Team" },
+    { id: 36, name: "Ansaf", position: "Media Lead", photo: "assets/team/Mohammed Ansaf K (media lead).jpg", team: "Media Team" },
+    { id: 37, name: "Faraha", position: "Media Lead", photo: "assets/team/Faraha Thariq -Media Lead.jpg", team: "Media Team" },
+    { id: 38, name: "Ayush", position: "Web Lead", photo: "assets/team/Aayush.jpeg", team: "Web Team" },
 
-    { id: 37, name: "Haro", position: "Media Team", photo: "assets/team/haro.jpg", team: "Media Team" },
-    { id: 38, name: "Aysha", position: "Media Team", photo: "assets/team/aysha.jpg", team: "Media Team" },
-    { id: 39, name: "Rohith", position: "Media Team", photo: "assets/team/rohith.jpg", team: "Media Team" },
-    { id: 40, name: "Alfred", position: "Media Team", photo: "assets/team/alfred.jpg", team: "Media Team" },
-    { id: 41, name: "Ansa", position: "Media Team", photo: "assets/team/ansa.jpg", team: "Media Team" },
+    { id: 39, name: "Hari", position: "Media Team", photo: "assets/team/Hari krishnan -Media.jpg", team: "Media Team" },
+    { id: 40, name: "Ayisha", position: "Media Team", photo: "assets/team/hiba.jpeg", team: "Media Team" },
+    { id: 41, name: "Rohith", position: "Media Team", photo: "assets/team/rohith.jpg", team: "Media Team" },
+    { id: 42, name: "Alfred", position: "Media Team", photo: "assets/team/alfred.jpg", team: "Media Team" },
+    { id: 43, name: "Anisa", position: "Media Team", photo: "assets/team/anisa.jpg", team: "Media Team" },
 
     // Web
-    { id: 42, name: "Nayana", position: "Web Team", photo: "assets/team/nayana.jpg", team: "Web Team" },
-    { id: 43, name: "Aparna", position: "Web Team", photo: "assets/team/aparna.jpg", team: "Web Team" },
-    { id: 44, name: "Isha", position: "Web Team", photo: "assets/team/isha.jpg", team: "Web Team" },
+    { id: 44, name: "Nayana", position: "Web Team", photo: "assets/team/nayana.jpg", team: "Web Team" },
+    { id: 45, name: "Aparna", position: "Web Team", photo: "assets/team/Aparna Suresh_web.jpg", team: "Web Team" },
+    { id: 46, name: "Esha", position: "Web Team", photo: "assets/team/esha.jpg", team: "Web Team" },
 
     /* ============================
            PROGRAM COMMITTEE
     ============================ */
-    { id: 45, name: "Arel", position: "Program Committee", photo: "assets/team/arel.jpg", team: "Program Committee" },
-    { id: 46, name: "Gomika", position: "Program Committee", photo: "assets/team/gomika.jpg", team: "Program Committee" },
-    { id: 47, name: "Rohit", position: "Program Committee", photo: "assets/team/rohit.jpg", team: "Program Committee" },
-    { id: 48, name: "Mariyam", position: "Program Committee", photo: "assets/team/mariyam.jpg", team: "Program Committee" },
-    { id: 49, name: "Davis", position: "Program Committee", photo: "assets/team/davis.jpg", team: "Program Committee" },
-    { id: 50, name: "Abhishek", position: "Program Committee", photo: "assets/team/abhishek.jpg", team: "Program Committee" },
-    { id: 51, name: "Ferin", position: "Program Committee", photo: "assets/team/ferin.jpg", team: "Program Committee" },
-    { id: 52, name: "Helen", position: "Program Committee", photo: "assets/team/helen.jpg", team: "Program Committee" },
-    { id: 53, name: "Shalen", position: "Program Committee", photo: "assets/team/shalen.jpg", team: "Program Committee" },
-    { id: 54, name: "Anul", position: "Program Committee", photo: "assets/team/anul.jpg", team: "Program Committee" },
-    { id: 55, name: "Aarhon", position: "Program Committee", photo: "assets/team/aarhon.jpg", team: "Program Committee" },
-    { id: 56, name: "Thejasree", position: "Program Committee", photo: "assets/team/thejasree.jpg", team: "Program Committee" },
-    { id: 57, name: "Alushree", position: "Program Committee", photo: "assets/team/alushree.jpg", team: "Program Committee" }
+    { id: 47, name: "Abel", position: "Program Committee", photo: "assets/team/Abel Shabi - PC.jpg", team: "Program Committee" },
+    { id: 48, name: "Gopika", position: "Program Committee", photo: "assets/team/gopika.jpg", team: "Program Committee" },
+    { id: 49, name: "Rohit", position: "Program Committee", photo: "assets/team/Rohit sreelal-pc.jpg", team: "Program Committee" },
+    { id: 50, name: "Mariam", position: "Program Committee", photo: "assets/team/mariam.jpg", team: "Program Committee" },
+    { id: 51, name: "Davis", position: "Program Committee", photo: "assets/team/DAVIS T JOSEPH _PC.jpg", team: "Program Committee" },
+    { id: 52, name: "Abhishek C S", position: "Program Committee", photo: "assets/team/Abhishek C S - PC - Copy.JPG", team: "Program Committee" },
+    { id: 53, name: "Febin", position: "Program Committee", photo: "assets/team/Febin Firos - PC.jpg", team: "Program Committee" },
+    { id: 54, name: "Helen", position: "Program Committee", photo: "assets/team/Helen Kurias-PC.jpg", team: "Program Committee" },
+    { id: 55, name: "Shalen", position: "Program Committee", photo: "assets/team/shalen.jpg", team: "Program Committee" },
+    { id: 56, name: "Amal", position: "Program Committee", photo: "assets/team/Amal Narayan -pc.webp", team: "Program Committee" },
+    { id: 57, name: "Abhinav", position: "Program Committee", photo: "assets/team/abhinav.jpg", team: "Program Committee" },
+    { id: 58, name: "Theajasree", position: "Program Committee", photo: "assets/team/theajasree.jpg", team: "Program Committee" },
+    { id: 59, name: "Abhishek", position: "Program Committee", photo: "assets/team/ABHISHEK S (PC) - Copy (6).jpg", team: "Program Committee" }
 ];
